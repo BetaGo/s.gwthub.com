1ca8e7ddb0407d5a449e07e97eb3e27829e977ed
BetaGo <BetaGo@users.noreply.github.com>
2020-06-28 15:54:41 +0000

    deploy: 9e8773bdbab4a1fba908d6d4609a99e2b9274910

---
+++ precache-manifest.362ad528107b696e4c722dbadfd01407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83cd137df0df4b154f202cf7d10aa9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e4aa1f327e49af0bdbb",
+    "url": "/static/css/main.b098cd40.chunk.css"
+  },
+  {
+    "revision": "9d55720616af9093919a",
+    "url": "/static/js/2.d4fea468.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4fea468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4aa1f327e49af0bdbb",
+    "url": "/static/js/main.a8301277.chunk.js"
+  },
+  {
+    "revision": "0c1802560254d7d264a6",
+    "url": "/static/js/runtime-main.89d65405.js"
+  }
+]);
