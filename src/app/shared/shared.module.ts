46c4c3e645971cf0c98d8f17a0657bc87fdfb918
BetaGo <gouwantong@live.com>
2020-07-15 22:57:10 +0800

    feat: search

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-
-
 
 @NgModule({
   declarations: [SearchBarComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, FormsModule],
+  exports: [SearchBarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
