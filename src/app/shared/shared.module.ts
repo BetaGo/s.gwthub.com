06eaa4c286201d8e3fba213f0e0fde7658d007c1
BetaGo <gouwantong@live.com>
2020-07-16 00:07:57 +0800

    feat: update style

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [SearchBarComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   exports: [SearchBarComponent],
 })
 export class SharedModule {}
